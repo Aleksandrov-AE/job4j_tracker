09327e43a820f4b1e70733534541877ee215923d
Tops <interfanspb@gmail.com>
2022-05-20 16:56:59 +0300

    0.4. Пользовательские исключения. [#219367 #302778]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,26 +2,23 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if (value == null) {
-            throw new ElementNotFoundException("Value cannot be null ");
-        }
-        if (key == null) {
-            throw new ElementNotFoundException("Key cannot be null ");
-        }
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (key.equals(value[i])) {
                 rsl = i;
+                break;
             }
-
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element Not Found");
         }
         return rsl;
     }
 
     public static void main(String[] args) {
         try {
-            String[] value = null;
-            String key = "";
+            String[] value = new String[] {"Tops", "Inter", "pinochet"};
+            String key = "Tops";
             System.out.println(indexOf(value, key));
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
