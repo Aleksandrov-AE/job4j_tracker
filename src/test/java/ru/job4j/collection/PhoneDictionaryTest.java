b7286e015a212d3598bf698e1aaceb3a1075f56a
Anatoliy Alexandrov <interfanspb@gmail.com>
2022-08-25 12:26:01 +0300

    1. Телефонный справочник на базе ArrayLis[#41598]

---
+++ src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertEquals(persons.get(0).getSurname(), "Arsentev");
+    }
+
+    @Test
+    public void whenFindByNameNull() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Kosta");
+        assertEquals(persons.size(), 0);
+    }
+}
