4f4739c15b117c6894178b5a48c9f501511e0873
Anatoliy Alexandrov <interfanspb@gmail.com>
2022-08-25 20:19:27 +0300

    2. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/test/java/ru/job4j/collection/PriorityQueueTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPrioritySecond() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertEquals(result.getDesc(), "urgent");
+    }
+
+    @Test
+    public void whenHigherPriorityEquals() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 5));
+        queue.put(new Task("middle", 5));
+        Task result = queue.take();
+        assertEquals(result.getDesc(), "low");
+    }
+}
