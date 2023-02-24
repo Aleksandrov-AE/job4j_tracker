9f79f8bf4e73bcbd7e0b525fa4822bced9cb26ad
Alex <alexandrov.a-e@yandex.ru>
2023-02-24 05:02:24 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]*

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,76 +1,58 @@
 package ru.job4j.early;
 
 public class PasswordValidator {
-    private static boolean containUpperLetter(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isUpperCase(s.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean containLowerLetter(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isLowerCase(s.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean containDigit(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean containSpecialSymbol(String s) {
-        String specialCharacters = "!@#$%&*()'+,-./:;<=>?@[\\]^_`{|}~";
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isLetterOrDigit(s.charAt(i))
-                    && specialCharacters.contains(String.valueOf(s.charAt(i)))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-     private static boolean containSubstringIgnore(String s) {
-        String[] substrings = {"qwerty", "12345", "password", "admin", "user"};
-         for (String substring: substrings) {
-             if (s.toLowerCase().contains(substring)) {
-                 return true;
-             }
-         }
-         return false;
-     }
 
      public static String validate(String password) {
+        boolean containLowerLetter = false;
+        boolean containUpperLetter =false;
+        boolean containDigit = false;
+        boolean containSpecialSymbol = false;
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (!containUpperLetter(password)) {
-            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-        }
-        if (!containLowerLetter(password)) {
+         for (int i = 0; i < password.length(); i++) {
+             if (Character.isLowerCase(password.charAt(i))) {
+                 containLowerLetter = true;
+             }
+             if (Character.isUpperCase(password.charAt(i))) {
+                 containUpperLetter = true;
+             }
+             if (Character.isDigit(password.charAt(i))) {
+                 containDigit = true;
+             }
+             if (!Character.isLetterOrDigit(password.charAt(i))
+                     && !Character.isDigit(password.charAt(i))) {
+                 containSpecialSymbol = true;
+             }
+         }
+         if (!containUpperLetter) {
+             throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+         }
+        if (!containLowerLetter) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
-        if (!containDigit(password)) {
+        if (!containDigit) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
-        if (!containSpecialSymbol(password)) {
+        if (!containSpecialSymbol) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
         if (containSubstringIgnore(password)) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
         }
         return password;
+    }
+
+    private static boolean containSubstringIgnore(String s) {
+        String[] substrings = {"qwerty", "12345", "password", "admin", "user"};
+        for (String substring: substrings) {
+            if (s.toLowerCase().contains(substring)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
